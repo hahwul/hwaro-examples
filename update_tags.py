afca1df0aa1b5f03aab813e453299d3ffb1f1096
hahwul <hahwul@gmail.com>
2026-04-07 17:28:12 +0900

    remove temps
    Signed-off-by: hahwul <hahwul@gmail.com>

--- update_tags.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("tags.json", "r") as f:
-    tags = json.load(f)
-
-tags["boutique"] = ["light", "store", "fashion", "elegant"]
-
-with open("tags.json", "w") as f:
-    json.dump(tags, f, indent=2)
