8b2289c14a9b753582b6522297719337d9af2432
hahwul <hahwul@gmail.com>
2026-04-18 18:14:58 +0900

    remove tmp files
    Signed-off-by: hahwul <hahwul@gmail.com>

--- add_tag.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('tags.json', 'r') as f:
-    tags = json.load(f)
-
-tags['astral-prism'] = ["dark", "glassmorphism", "portfolio", "prismatic", "cosmic"]
-
-with open('tags.json', 'w') as f:
-    json.dump(tags, f, indent=2)
